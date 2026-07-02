e26aa4855af7ba94b8f9c4c66478eb0b3aaf5ceb
Edson Chambi Quispe <edsonchambi2000@gmail.com>
2026-07-02 13:33:25 -0400

    inicializacion del proyecto

---
+++ src/main/java/com/FORCE/FORCE_reservas_backend/ForceReservasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.FORCE.FORCE_reservas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForceReservasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForceReservasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FORCE/FORCE_reservas_backend/ForceReservasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FORCE.FORCE_reservas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForceReservasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
